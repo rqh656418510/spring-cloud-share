5a9b635bf0388864443abcb6fddfe0857f39d52d
rqh656418510 <656418510@qq.com>
2022-08-21 17:54:41 +0800

    Update files

---
+++ kafka-study/kafka-lesson-1/src/main/java/com/clay/kafka/producer/CustomerProducer.java
@@ -0,0 +1,33 @@
+package com.clay.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * @author clay
+ */
+public class CustomerProducer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 指定Kafka集群的连接信息
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092,127.0.0.1:9093");
+        // 指定序列化器（必需）
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 创建生产者对象
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for (int i = 0; i < 5; i++) {
+            // 发送消息
+            producer.send(new ProducerRecord<>("test", "hello kafka " + i));
+        }
+        // 关闭资源
+        producer.close();
+    }
+
+}
