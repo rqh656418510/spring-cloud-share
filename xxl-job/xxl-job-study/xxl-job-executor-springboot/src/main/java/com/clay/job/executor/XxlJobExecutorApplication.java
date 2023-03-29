f01a3c6112b75e2e305b618c09ffb87662702d9e
rqh656418510 <656418510@qq.com>
2023-03-29 20:17:42 +0800

    Update files

---
+++ xxl-job/xxl-job-study/xxl-job-executor-springboot/src/main/java/com/clay/job/executor/XxlJobExecutorApplication.java
@@ -0,0 +1,16 @@
+package com.clay.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author clay
+ */
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+    }
+
+}
