60f57ef24ae2b0c6571bc1469b8621973a9a65f9
rqh656418510 <656418510@qq.com>
2023-06-19 14:10:33 +0800

    Update files

---
+++ spring-boot-3/spring-boot-3-study/spring-boot3-04/src/main/java/com/clay/boot/web/MainApplication.java
@@ -0,0 +1,16 @@
+package com.clay.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author clay
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
