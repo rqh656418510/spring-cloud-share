5b2237e77ac7f29b8650b2ff1462413f41d59183
rqh656418510 <656418510@qq.com>
2023-06-29 21:20:30 +0800

    Update files

--- spring-boot-3/spring-boot-3-study/spring-boot3-04/src/main/java/com/clay/boot/web/controller/PersonController.java
+++ spring-boot-3/spring-boot-3-study/spring-boot3-04/src/main/java/com/clay/boot/web/controller/PersonController.java
@@ -2,9 +2,13 @@ package com.clay.boot.web.controller;
 
 import com.clay.boot.web.domain.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/person")
@@ -16,9 +20,22 @@ public class PersonController {
         Person person = new Person();
         person.setId(1L);
         person.setAge(18);
+        person.setRole("admin");
         person.setUserName("张三");
         person.setEmail("aaa@qq.com");
         return person;
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person(1L, "Peter1", "peter@gmail.com", 18, "pm"));
+        persons.add(new Person(2L, "Peter2", "peter@gmail.com", 16, "admin"));
+        persons.add(new Person(3L, "Peter3", "peter@gmail.com", 18, "pm"));
+        persons.add(new Person(4L, "Peter4", "peter@gmail.com", 17, "hr"));
+        persons.add(new Person(5L, "Peter5", "peter@gmail.com", 18, "boss"));
+        model.addAttribute("persons", persons);
+        return "list";
     }
 
 }
--- spring-boot-3/spring-boot-3-study/spring-boot3-04/src/main/java/com/clay/boot/web/domain/Person.java
+++ spring-boot-3/spring-boot-3-study/spring-boot3-04/src/main/java/com/clay/boot/web/domain/Person.java
@@ -1,9 +1,13 @@
 package com.clay.boot.web.domain;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JacksonXmlRootElement
 public class Person {
 
@@ -11,5 +15,6 @@ public class Person {
     private String userName;
     private String email;
     private Integer age;
+    private String role;
 
 }
