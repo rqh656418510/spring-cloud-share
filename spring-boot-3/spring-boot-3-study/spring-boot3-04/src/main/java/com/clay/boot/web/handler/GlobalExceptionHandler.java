7e0de52bf6e610ae044abca70f7156e1c39703d0
rqh656418510 <656418510@qq.com>
2023-07-01 15:08:20 +0800

    Update files

---
+++ spring-boot-3/spring-boot-3-study/spring-boot3-04/src/main/java/com/clay/boot/web/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.clay.boot.web.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception exception) {
+        return "Inner Error: " + exception.getMessage();
+    }
+
+}
