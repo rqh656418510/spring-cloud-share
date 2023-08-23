420ddeb6b33bd53b9be443e2d6f8a53cecc4a99b
rqh656418510 <656418510@qq.com>
2023-08-23 18:47:58 +0800

    Update files

--- spring-boot-3/spring-boot-3-study/spring-boot3-11/src/main/java/com/clay/boot/config/RedisConfiguration.java
+++ spring-boot-3/spring-boot-3-study/spring-boot3-11/src/main/java/com/clay/boot/config/RedisConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @author clay
@@ -13,16 +15,40 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 public class RedisConfiguration {
 
     /**
-     * 允许 Object 类型的 Key-Value 都可以被转换为 Json 字符串进行存储
-     * @param redisConnectionFactory 自动配置好的连接工厂
-     * @return
+     * 设置序列化器
+     */
+    private void setSerializer(RedisTemplate template) {
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        // Key采用String的序列化方式
+        template.setKeySerializer(new StringRedisSerializer());
+        // Value序列化方式采用Jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // Hash的Key采用String的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        // Hash的Value序列化方式采用Jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+    }
+
+    /**
+     * StringRedisTemplate
      */
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
         template.setConnectionFactory(redisConnectionFactory);
-        // 设置默认序列化器，将对象转换为Json字符串的序列化工具
-        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        setSerializer(template);
+        return template;
+    }
+
+    /**
+     * RedisTemplate
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        setSerializer(template);
+        template.afterPropertiesSet();
         return template;
     }
 
