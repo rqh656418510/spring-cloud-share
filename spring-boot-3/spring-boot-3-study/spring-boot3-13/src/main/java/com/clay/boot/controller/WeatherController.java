5f7a55fc990b479633e74df9bcc8700757744bcd
rqh656418510 <656418510@qq.com>
2023-08-24 19:20:20 +0800

    Update files

---
+++ spring-boot-3/spring-boot-3-study/spring-boot3-13/src/main/java/com/clay/boot/controller/WeatherController.java
@@ -0,0 +1,21 @@
+package com.clay.boot.controller;
+
+import com.clay.boot.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/weather")
+    public Mono<String> weather(@RequestParam(name = "city") String city) {
+        return weatherService.weather(city);
+    }
+
+}
---
+++ spring-boot-3/spring-boot-3-study/spring-boot3-13/src/main/java/com/clay/boot/service/WeatherService.java
@@ -0,0 +1,29 @@
+package com.clay.boot.service;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class WeatherService {
+
+    public Mono<String>  weather(String city) {
+        // 请求参数
+        Map<String, String> params = new HashMap<>();
+        params.put("area", city);
+
+        // 发送请求（异步）
+        WebClient client = WebClient.create();
+        return client.get()
+            .uri("https://ali-weather.showapi.com/area-to-weather-date?area={area}", params)
+            .header("Authorization", "APPCODE 93b7e19861a24c519a7548b17dc16d75")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(String.class);
+    }
+
+}
