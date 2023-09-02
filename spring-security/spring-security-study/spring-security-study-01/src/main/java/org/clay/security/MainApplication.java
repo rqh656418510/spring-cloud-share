429be9def55f89d4cb79aadda56fea6efdad45f2
rqh656418510 <656418510@qq.com>
2023-09-02 10:21:05 +0800

    Update files

---
+++ spring-security/spring-security-study/spring-security-study-01/src/main/java/org/clay/security/MainApplication.java
@@ -0,0 +1,16 @@
+package org.clay.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author clay
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
