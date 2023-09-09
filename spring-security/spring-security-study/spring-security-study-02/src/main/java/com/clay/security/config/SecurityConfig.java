256de79f5e0f8abc766d010ff425705215ed89bf
rqh656418510 <656418510@qq.com>
2023-09-09 22:51:40 +0800

    Update code

---
+++ spring-security/spring-security-study/spring-security-study-02/src/main/java/com/clay/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.clay.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author clay
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodePassword = passwordEncoder.encode("123456");
+        auth.inMemoryAuthentication().withUser("admin").password(encodePassword).roles("manager");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
