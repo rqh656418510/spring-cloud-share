03791fe3843a40643c55974c8c32686837814556
rqh656418510 <656418510@qq.com>
2023-09-13 20:50:45 +0800

    Update files

--- spring-security/spring-security5-study/spring-security5-study-05/src/main/java/com/clay/security/config/SecurityConfig.java
+++ spring-security/spring-security5-study/spring-security5-study-05/src/main/java/com/clay/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+            .loginPage("/login.html")           // 配置哪个 URL 为登录页面
+            .loginProcessingUrl("/user/login")  // 配置哪个为登录接口的 URL
+            .defaultSuccessUrl("/hello")        // 配置登录成功之后跳转到哪个 URL
+            .and().authorizeRequests()
+            .antMatchers("/hello").hasAuthority("manager")        // 需要用户拥有 manager 权限才能访问对应的 URL
+            .antMatchers("/", "/login.html", "/user/login").permitAll()     // 配置哪些 URL 不需要登录就可以直接访问
+            .anyRequest().authenticated()      // 配置其他 URL 需要登录才能访问
+            .and().csrf().disable();           // 关闭 CSRF 防护
     }
 
     @Bean
--- spring-security/spring-security5-study/spring-security5-study-06/src/main/java/com/clay/security/controller/HelloController.java
+++ spring-security/spring-security5-study/spring-security5-study-06/src/main/java/com/clay/security/controller/HelloController.java
@@ -1,6 +1,6 @@
 package com.clay.security.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String hello() {
         return "Hello Spring Security";
     }
