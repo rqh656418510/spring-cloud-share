f23f7a34e0d9c4a062ab38b79205622e4569f774
rqh656418510 <656418510@qq.com>
2023-09-13 19:47:18 +0800

    Update files

--- spring-security/spring-security5-study/spring-security5-study-06/src/main/java/com/clay/security/MainApplication.java
+++ spring-security/spring-security5-study/spring-security5-study-06/src/main/java/com/clay/security/MainApplication.java
@@ -1,5 +1,6 @@
 package com.clay.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author clay
  */
 @SpringBootApplication
+@MapperScan("com.clay.security.**.mapper")
 public class MainApplication {
 
     public static void main(String[] args) {
--- spring-security/spring-security5-study/spring-security5-study-06/src/main/java/com/clay/security/service/LoginServiceImpl.java
+++ spring-security/spring-security5-study/spring-security5-study-06/src/main/java/com/clay/security/service/LoginServiceImpl.java
@@ -1,12 +1,14 @@
 package com.clay.security.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.clay.security.entity.User;
+import com.clay.security.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,12 +19,20 @@ import java.util.List;
 @Service("userDetailsService")
 public class LoginServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 查询数据库
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUsername, username);
+        User userEntity = userMapper.selectOne(queryWrapper);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("User name not exist");
+        }
         List<GrantedAuthority> authors = AuthorityUtils.commaSeparatedStringToAuthorityList("manager");
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodePassword = passwordEncoder.encode("123456");
-        return new User("admin", encodePassword, authors);
+        return new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(), authors);
     }
 
 }
