bf1e122ea6274873545492349d69e67e6e8c1a4c
rqh656418510 <656418510@qq.com>
2023-10-12 09:53:24 +0800

    Update files

---
+++ spring-security/spring-security5-study/spring-security5-10/infrastructure/api-gateway/src/main/java/com/clay/api/gateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.clay.api.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * 跨域配置
+ *
+ * @author clay
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+}
