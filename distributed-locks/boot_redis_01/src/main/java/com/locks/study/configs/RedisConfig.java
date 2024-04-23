7a171503b1ca06f9425e2a2d3a72ad42ab8f5e03
rqh656418510 <656418510@qq.com>
2024-04-23 11:54:22 +0800

    feat: update code files

---
+++ distributed-locks/boot_redis_01/src/main/java/com/locks/study/configs/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.locks.study.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        // 新建 RedisTemplate 对象，key 为 String 对象，value 为 Serializable（可序列化的）对象
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        // key 值使用字符串序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // value 值使用 json 序列化器
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // 传入连接工厂
+        redisTemplate.setConnectionFactory(connectionFactory);
+        // 返回 redisTemplate 对象
+        return redisTemplate;
+    }
+
+}
