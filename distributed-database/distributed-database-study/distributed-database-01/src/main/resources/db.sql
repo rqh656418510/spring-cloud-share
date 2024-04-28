51b29889c59cec2a493f9089577fef993a289f3f
rqh656418510 <656418510@qq.com>
2024-04-28 22:01:16 +0800

    feat: update code files

--- distributed-database/distributed-database-study/distributed-database-01/src/main/resources/db.sql
+++ distributed-database/distributed-database-study/distributed-database-01/src/main/resources/db.sql
@@ -1,3 +1,9 @@
+create database distributed_database_study_01 default character set utf8;
+create database distributed_database_study_02 default character set utf8;
+create database distributed_database_study_03 default character set utf8;
+create database distributed_database_study_04 default character set utf8;
+create database distributed_database_study_05 default character set utf8;
+
 create table `user` (
     `id` bigint(20) not null auto_increment,
     `username` varchar(25) default null,
