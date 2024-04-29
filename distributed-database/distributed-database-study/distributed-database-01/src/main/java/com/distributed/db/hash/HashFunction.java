c7f32a247cc3e85fde07ef95f72a2c6617da5457
rqh656418510 <656418510@qq.com>
2024-04-29 19:22:12 +0800

    feat: update code files

--- distributed-database/distributed-database-study/distributed-database-01/src/main/java/com/distributed/db/hash/HashFunction.java
+++ distributed-database/distributed-database-study/distributed-database-01/src/main/java/com/distributed/db/hash/HashFunction.java
@@ -1,11 +1,21 @@
 package com.distributed.db.hash;
 
+/**
+ * 根据某种自定义的Hash算法来进行散列,并根据散列的值进行路由
+ * 常见的水平切分规则有:
+ * 基于范围的切分, 比如 memberId > 10000 and memberId < 20000
+ * 基于模数的切分, 比如 memberId%128==1 或者 memberId%128==2 或者...
+ * 基于哈希(hashing)的切分, 比如hashing(memberId)==someValue等
+ */
 public class HashFunction {
 
+    /**
+     * 对三个主数据库进行散列分布
+     * <p> 返回其他值，没有在配置文件中配置的，如负数等，则在默认数据库中查找
+     */
     public int applyEmployee(Long employeeId) {
         int result = Math.abs(employeeId.hashCode() % 1024);
         System.out.println("hash : " + result);
-
         if (0 <= result && result < 341) {
             result = 1;
             System.out.println("在第1个数据库中");
