11573f0800527909a276f55dd87211cc754ff620
rqh656418510 <656418510@qq.com>
2024-05-14 21:52:29 +0800

    Update files

--- juc/juc-study/juc-lesson-01/src/main/java/com/java/jmm/VolatileTest1.java
+++ juc/juc-study/juc-lesson-01/src/main/java/com/java/jmm/VolatileTest1.java
@@ -2,6 +2,9 @@ package com.java.jmm;
 
 /**
  * volatile 解决内存可见性问题
+ *
+ * <p> 内存可见性问题是指：多个线程操作共享数据时，彼此不可见
+ * <p> volatile 三大特性：保证可见性、禁止指令重排、不保证原子性
  */
 class ThreadDemo1 implements Runnable {
 
@@ -36,7 +39,7 @@ public class VolatileTest1 {
         // 主线程读取共享数据
         while (true) {
             if (threadDemo.isFlag()) {
-                System.out.println(Thread.currentThread().getName() + " flag = " + threadDemo.isFlag());
+                System.out.println("-----" + Thread.currentThread().getName() + " flag = " + threadDemo.isFlag());
                 break;
             }
         }
--- juc/juc-study/juc-lesson-01/src/main/java/com/java/jmm/VolatileTest2.java
+++ juc/juc-study/juc-lesson-01/src/main/java/com/java/jmm/VolatileTest2.java
@@ -2,6 +2,8 @@ package com.java.jmm;
 
 /**
  * synchonrized 解决内存可见性问题
+ *
+ * <p> 内存可见性问题是指：多个线程操作共享数据时，彼此不可见
  */
 class ThreadDemo2 implements Runnable {
 
@@ -34,10 +36,10 @@ public class VolatileTest2 {
 
         // 主线程读取共享数据
         while (true) {
-            // 使用 synchronized 同步机制
+            // 使用 synchronized 同步机制保证可见性（效率非常低）
             synchronized (threadDemo) {
                 if (threadDemo.isFlag()) {
-                    System.out.println(Thread.currentThread().getName() + " flag = " + threadDemo.isFlag());
+                    System.out.println("-----" + Thread.currentThread().getName() + " flag = " + threadDemo.isFlag());
                     break;
                 }
             }
