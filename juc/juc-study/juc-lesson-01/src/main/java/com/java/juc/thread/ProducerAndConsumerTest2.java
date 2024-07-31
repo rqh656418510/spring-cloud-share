cbd49df82df43ceb2600c84032cab08aebee66e4
rqh656418510 <656418510@qq.com>
2024-07-31 16:45:51 +0800

    Update files

--- juc/juc-study/juc-lesson-01/src/main/java/com/java/juc/thread/ProducerAndConsumerTest2.java
+++ juc/juc-study/juc-lesson-01/src/main/java/com/java/juc/thread/ProducerAndConsumerTest2.java
@@ -1,11 +1,15 @@
 package com.java.juc.thread;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- * 线程通信之生产者消费者模式（Synchonrized 版本实现）
+ * 线程通信之生产者消费者模式（Condition 版本实现）
  *
  * <p> 一个初始值为零的变量，两个线程对其交替操作，一个线程加 1，一个线程减 1，重复 5 轮操作。
  */
-public class ProducerAndConsumerTest {
+public class ProducerAndConsumerTest2 {
 
     public static void main(String[] args) {
         Clerk2 clerk = new Clerk2();
@@ -21,45 +25,55 @@ public class ProducerAndConsumerTest {
 /**
  * 店员
  */
-class Clerk {
+class Clerk2 {
 
     private int product = 0;
+
+    private Lock locker = new ReentrantLock();
+
+    private Condition condition = locker.newCondition();
 
     /**
      * 进货
      */
-    public synchronized void stock() {
+    public void stock() {
+        locker.lock();
         try {
             // 使用 while 循环，而不是使用 if 判断，否则可能会导致虚假唤醒的现象
             while (product != 0) {
                 // 等待
-                this.wait();
+                condition.await();
             }
             ++product;
             System.out.println(Thread.currentThread().getName() + " : " + product);
             // 唤醒
-            this.notifyAll();
+            condition.signalAll();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            locker.unlock();
         }
     }
 
     /**
      * 卖货
      */
-    public synchronized void sale() {
+    public void sale() {
+        locker.lock();
         try {
             // 使用 while 循环，而不是使用 if 判断，否则可能会导致虚假唤醒的现象
             while (product == 0) {
                 // 等待
-                this.wait();
+                condition.await();
             }
             --product;
             System.out.println(Thread.currentThread().getName() + " : " + product);
             // 唤醒
-            this.notifyAll();
+            condition.signalAll();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            locker.unlock();
         }
     }
 
@@ -68,11 +82,11 @@ class Clerk {
 /**
  * 生产者
  */
-class Producer implements Runnable {
+class Producer2 implements Runnable {
 
     private Clerk2 clerk;
 
-    public Producer(Clerk2 clerk) {
+    public Producer2(Clerk2 clerk) {
         this.clerk = clerk;
     }
 
@@ -89,11 +103,11 @@ class Producer implements Runnable {
 /**
  * 消费者
  */
-class Consumer implements Runnable {
+class Consumer2 implements Runnable {
 
     private Clerk2 clerk;
 
-    public Consumer(Clerk2 clerk) {
+    public Consumer2(Clerk2 clerk) {
         this.clerk = clerk;
     }
 
