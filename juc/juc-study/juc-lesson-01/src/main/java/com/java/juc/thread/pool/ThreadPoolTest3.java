be6df1c5ddbaee870dc09d420894969e493cda2b
rqh656418510 <656418510@qq.com>
2024-08-01 16:33:40 +0800

    Update files

---
+++ juc/juc-study/juc-lesson-01/src/main/java/com/java/juc/thread/pool/ThreadPoolTest3.java
@@ -0,0 +1,41 @@
+package com.java.juc.thread.pool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池的使用
+ */
+public class ThreadPoolTest3 {
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建线程池
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+
+        // 延迟执行任务（延迟 3 秒执行），任务只会执行一次
+        // executorService.schedule(new MyRunnable3(), 1, TimeUnit.SECONDS);
+
+        // 以固定频率调用指定的任务（延迟5秒后执行，每隔3秒执行一次）
+        // executorService.scheduleAtFixedRate(new MyRunnable3(), 5, 3, TimeUnit.SECONDS);
+
+        // 当任务执行完毕后，让其延迟固定时间后再次运行（延迟5秒后执行，每隔3秒执行一次）
+        executorService.scheduleWithFixedDelay(new MyRunnable3(), 5, 3, TimeUnit.SECONDS);
+
+        TimeUnit.MINUTES.sleep(1);
+
+        // 关闭线程池
+        executorService.shutdown();
+    }
+
+}
+
+class MyRunnable3 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        throw new RuntimeException();
+    }
+
+}
