2e77b19763b5b03ffd6016952034ee982030cd46
rqh656418510 <656418510@qq.com>
2024-08-17 20:11:57 +0800

    Update files

--- spring-cloud/spring-cloud-study-2024/cloud-provoder-payment/src/main/java/com/turing/cloud/exception/GlobalExceptionHandler.java
+++ spring-cloud/spring-cloud-study-2024/cloud-provoder-payment/src/main/java/com/turing/cloud/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.turing.cloud.resp.ResultData;
 import com.turing.cloud.resp.ReturnCodeEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,9 +19,20 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResultData<String> exception(Exception e) {
-        log.error("全局异常信息：{}", e.getMessage(), e);
-        return ResultData.fail(ReturnCodeEnum.RC500.getCode(), e.getMessage());
+    public ResultData<String> exceptionHandler(Exception e) {
+        // 日志记录
+        log.error("Global exception message：{}", e.getMessage(), e);
+
+        if (e instanceof GlobalException) {
+            // 处理自定义的全局异常
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(), e.getMessage());
+        } else if (e instanceof BindException) {
+            // 处理基于SpringMVC的JSR303参数校验失败的情况
+            return ResultData.fail(ReturnCodeEnum.RC380.getCode(), e.getMessage());
+        } else {
+            // 处理其他未知的全局异常
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(), e.getMessage());
+        }
     }
 
 }
