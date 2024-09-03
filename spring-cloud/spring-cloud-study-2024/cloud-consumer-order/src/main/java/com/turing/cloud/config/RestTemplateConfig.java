c600ddaf636636005c48a8c969ae7708961666dd
rqh656418510 <656418510@qq.com>
2024-09-04 01:41:15 +0800

    Update files

--- spring-cloud/spring-cloud-study-2024/cloud-consumer-order/src/main/java/com/turing/cloud/config/RestTemplateConfig.java
+++ spring-cloud/spring-cloud-study-2024/cloud-consumer-order/src/main/java/com/turing/cloud/config/RestTemplateConfig.java
@@ -1,8 +1,15 @@
 package com.turing.cloud.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -10,12 +17,23 @@ import org.springframework.web.client.RestTemplate;
  * @version 1.0
  */
 @Configuration
+// 下面的value值大小写敏感，一定要和Consul里面的服务名称一样
+@LoadBalancerClient(value = "cloud-payment-service", configuration = RestTemplateConfig.class)
 public class RestTemplateConfig {
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 将默认的负载均衡算法从轮询切换为随机
+     */
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment, LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
     }
 
 }
