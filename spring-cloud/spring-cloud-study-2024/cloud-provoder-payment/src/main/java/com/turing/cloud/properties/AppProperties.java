7b04594dfcdd10eafe96d935a8c6e8879452be45
rqh656418510 <656418510@qq.com>
2024-09-03 10:21:09 +0800

    Update files

--- spring-cloud/spring-cloud-study-2024/cloud-provoder-payment/src/main/java/com/turing/cloud/properties/AppProperties.java
+++ spring-cloud/spring-cloud-study-2024/cloud-provoder-payment/src/main/java/com/turing/cloud/properties/AppProperties.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppProperties {
 
-    @Value("${env.info}")
+    @Value("${env.info:}")
     private String info;
 
 }
