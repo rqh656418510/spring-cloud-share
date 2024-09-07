4db8defcbf869bbf13f4cb3750ffa1c46d896ab9
rqh656418510 <656418510@qq.com>
2024-09-07 20:36:00 +0800

    Update files

--- spring-cloud/spring-cloud-study-2024/cloud-consumer-feign-order/src/main/java/com/turing/cloud/config/FeignConfig.java
+++ spring-cloud/spring-cloud-study-2024/cloud-consumer-feign-order/src/main/java/com/turing/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.turing.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +15,12 @@ public class FeignConfig {
     @Bean
     public Retryer retryer() {
         // 初始间隔时间为100ms，重试最大间隔时间为1000ms，最大请求次数为3（1次初始调用，2次重试调用）
-        // return new Retryer.Default(100, 1000, 3);
+        return new Retryer.Default(100, 1000, 3);
+    }
 
-        // 配置不使用失败重试机制
-        return Retryer.NEVER_RETRY;
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
