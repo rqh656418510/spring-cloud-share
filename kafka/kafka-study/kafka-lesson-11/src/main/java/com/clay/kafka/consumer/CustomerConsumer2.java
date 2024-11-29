3f681d7ba0390c41965859a03d435d4f2a02aef9
rqh656418510 <656418510@qq.com>
2024-11-29 13:43:14 +0800

    Update files

---
+++ kafka/kafka-study/kafka-lesson-11/src/main/java/com/clay/kafka/consumer/CustomerConsumer2.java
@@ -0,0 +1,48 @@
+package com.clay.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * 消费者的分区分配策略 - Range
+ *
+ * @author clay
+ */
+public class CustomerConsumer2 {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 指定Kafka的连接信息（若是Kafka集群，多个节点之间使用逗号分隔）
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        // 指定反序列化器（必须）
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // 指定消费者组 ID（必须，可以任意定义）
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+
+        // 创建消费者对象
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // 订阅要消费的主题
+        List<String> topics = new ArrayList<>();
+        topics.add("first");
+        consumer.subscribe(topics);
+
+        while (true) {
+            // 设置每隔 1 秒消费一批数据
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord);
+            }
+        }
+    }
+
+}
