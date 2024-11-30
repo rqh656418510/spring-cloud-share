725d7828615d3d05936e4094316b7c0aec79914e
rqh656418510 <656418510@qq.com>
2024-11-30 23:08:44 +0800

    Update files

--- kafka/kafka-study/kafka-lesson-15/src/main/java/com/clay/kafka/consumer/CustomerConsumer1.java
+++ kafka/kafka-study/kafka-lesson-15/src/main/java/com/clay/kafka/consumer/CustomerConsumer1.java
@@ -12,11 +12,11 @@ import java.util.List;
 import java.util.Properties;
 
 /**
- * 消费者自动提交 offset
+ * 消费者手动提交 offset（同步提交）
  *
  * @author clay
  */
-public class CustomerConsumer {
+public class CustomerConsumer1 {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
@@ -27,10 +27,8 @@ public class CustomerConsumer {
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         // 指定消费者组 ID（必须，可以任意定义）
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-        // 是否自动提交 offset
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-        // 指定自动提交 offset 的时间间隔，默认值是 5 秒
-        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
+        // 关闭自动提交 offset
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 
         // 创建消费者对象
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
@@ -46,6 +44,8 @@ public class CustomerConsumer {
             for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
                 System.out.println(consumerRecord);
             }
+            // 同步提交 offset（会阻塞当前线程，并且有失败重试机制）
+            consumer.commitSync();
         }
     }
 
