6e41ec80851170ed611cc201c6e91624553232dc
rqh656418510 <656418510@qq.com>
2024-12-01 14:17:10 +0800

    Update files

---
+++ kafka/kafka-study/kafka-lesson-16/src/main/java/com/clay/kafka/consumer/CustomerConsumer.java
@@ -0,0 +1,64 @@
+package com.clay.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * 消费者指定 offset 消费
+ *
+ * @author clay
+ */
+public class CustomerConsumer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 指定Kafka的连接信息（若是Kafka集群，多个节点之间使用逗号分隔）
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        // 指定反序列化器（必须）
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // 指定消费者组 ID（必须，可以任意定义）
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test2");
+
+        // 创建消费者对象
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // 订阅要消费的主题
+        List<String> topics = new ArrayList<>();
+        topics.add("first");
+        consumer.subscribe(topics);
+
+        // 获取消费者的分区分配信息（有了分区分配信息才能开始消费）
+        Set<TopicPartition> assignment = new HashSet<>();
+        while (assignment.size() == 0) {
+            consumer.poll(Duration.ofSeconds(1));
+            // 获取消费者的分区分配信息
+            assignment = consumer.assignment();
+        }
+
+        // 遍历所有分区分配信息，并指定 offset 从 23 的位置开始消费
+        for (TopicPartition topicPartition : assignment) {
+            consumer.seek(topicPartition, 23);
+        }
+
+        while (true) {
+            // 设置每隔 1 秒消费一批数据
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord);
+            }
+        }
+    }
+
+}
---
+++ kafka/kafka-study/kafka-lesson-16/src/main/java/com/clay/kafka/consumer/CustomerProducer.java
@@ -0,0 +1,46 @@
+package com.clay.kafka.consumer;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * 消费者指定 offset 消费
+ *
+ * @author clay
+ */
+public class CustomerProducer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 指定Kafka的连接信息（若是Kafka集群，多个节点之间使用逗号分隔）
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        // 指定序列化器（必须）
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 创建生产者对象
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for (int i = 0; i < 7; i++) {
+            // 异步发送消息（带回调函数）
+            producer.send(new ProducerRecord<>("first", i, "", "hello kafka " + i), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception exception) {
+                    if (exception != null) {
+                        exception.printStackTrace();
+                    } else {
+                        System.out.println("topic: " + recordMetadata.topic() + ", partition: " + recordMetadata.partition());
+                    }
+                }
+            });
+        }
+        // 关闭资源
+        producer.close();
+    }
+
+}
