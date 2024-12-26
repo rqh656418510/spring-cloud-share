36a66c0b53120a2961bbd280a3d7ef5fe22b2b11
rqh656418510 <656418510@qq.com>
2024-12-26 17:13:40 +0800

    Update files

--- rpc-framework/dubbo-study/dubbo-lesson-04/dubbo-lesson-04-consumer/src/main/java/com/clay/dubbo/consumer/ConsumerApplication.java
+++ rpc-framework/dubbo-study/dubbo-lesson-04/dubbo-lesson-04-consumer/src/main/java/com/clay/dubbo/consumer/ConsumerApplication.java
@@ -1,11 +1,9 @@
 package com.clay.dubbo.consumer;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@DubboComponentScan(basePackages = "com.clay.dubbo.consumer")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- rpc-framework/dubbo-study/dubbo-lesson-04/dubbo-lesson-04-provider/src/main/java/com/clay/dubbo/provider/ProviderApplication.java
+++ rpc-framework/dubbo-study/dubbo-lesson-04/dubbo-lesson-04-provider/src/main/java/com/clay/dubbo/provider/ProviderApplication.java
@@ -1,11 +1,9 @@
 package com.clay.dubbo.provider;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@DubboComponentScan(basePackages = "com.clay.dubbo.provider.service")
 public class ProviderApplication {
 
     public static void main(String[] args) {
