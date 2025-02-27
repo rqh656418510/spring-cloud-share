ce7ceda755cc7887b40ee9585d2547c7aa4170c6
rqh656418510 <656418510@qq.com>
2025-02-27 12:43:15 +0800

    Update files

--- rpc-framework/dubbo-study/dubbo2.x-study/dubbo-lesson-07/dubbo-lesson-07-producer/src/main/java/com/clay/dubbo/producer/service/impl/UserServiceImpl.java
+++ rpc-framework/dubbo-study/dubbo2.x-study/dubbo-lesson-07/dubbo-lesson-07-producer/src/main/java/com/clay/dubbo/producer/service/impl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.apache.dubbo.config.annotation.DubboService;
 /**
  * 暴露 Dubbo 服务
  */
-@DubboService(protocol = "rest")
+@DubboService(protocol = {"dubbo", "rest"})
 public class UserServiceImpl implements UserService {
 
     @Override
