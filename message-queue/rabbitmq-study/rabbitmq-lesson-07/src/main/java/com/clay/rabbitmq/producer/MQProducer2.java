966c2f0c7a60bdcec3d6328cae50305e0897ab4d
rqh656418510 <656418510@qq.com>
2025-04-02 18:40:41 +0800

    Update files

---
+++ message-queue/rabbitmq-study/rabbitmq-lesson-07/src/main/java/com/clay/rabbitmq/producer/MQProducer2.java
@@ -0,0 +1,68 @@
+package com.clay.rabbitmq.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class MQProducer2 {
+
+    /**
+     * 批量确认发布
+     */
+    public static void publishMessageBatch() throws Exception {
+        // 创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.2.127");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+
+        try (
+            // 创建连接
+            Connection connection = factory.newConnection();
+            // 创建信道
+            Channel channel = connection.createChannel()
+        ) {
+            String queueName = UUID.randomUUID().toString();
+
+            // 声明队列
+            channel.queueDeclare(queueName, false, false, false, null);
+
+            // 开启发布确认
+            channel.confirmSelect();
+
+            // 批量确认消息数量
+            int batchSize = 100;
+
+            // 未确认消息个数
+            int outstandingMessageCount = 0;
+
+            int total = 1000;
+            long begin = System.currentTimeMillis();
+            for (int i = 0; i < total; i++) {
+                // 发布消息
+                String message = i + "";
+                channel.basicPublish("", queueName, null, message.getBytes(StandardCharsets.UTF_8));
+
+                // 批量确认发布
+                outstandingMessageCount++;
+                if (outstandingMessageCount == batchSize) {
+                    channel.waitForConfirms();
+                    outstandingMessageCount = 0;
+                }
+            }
+
+            // 为了确保没有剩余的未确认消息，再次确认发布
+            if (outstandingMessageCount > 0) {
+                channel.waitForConfirms();
+            }
+
+            long end = System.currentTimeMillis();
+            System.out.println("发布" + total + "个批量确认消息，耗时" + (end - begin) + "ms");
+        }
+    }
+
+}
---
+++ message-queue/rabbitmq-study/rabbitmq-lesson-07/src/main/java/com/clay/rabbitmq/producer/MQProducer3.java
@@ -0,0 +1,99 @@
+package com.clay.rabbitmq.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class MQProducer3 {
+
+    /**
+     * 异步确认发布
+     */
+    public static void publishMessageAsync() throws Exception {
+        // 创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.2.127");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+
+        try (
+            // 创建连接
+            Connection connection = factory.newConnection();
+            // 创建信道
+            Channel channel = connection.createChannel()
+        ) {
+            String queueName = UUID.randomUUID().toString();
+
+            // 声明队列
+            channel.queueDeclare(queueName, false, false, false, null);
+
+            // 开启发布确认
+            channel.confirmSelect();
+
+            /**
+             * 线程安全的一个有序哈希表，适用于高并发场景
+             * 1. 轻松地将消息序列号与消息进行关联
+             * 2. 轻松地批量删除条目，只要传入消息序列号
+             * 3. 支持并发访问
+             */
+            ConcurrentSkipListMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
+
+            /**
+             * 收到确认消息的回调
+             * 1. sequenceNumber 表示消息序列号
+             * 2. multiple = true，表示可以确认小于等于当前消息序列号的消息
+             * 3. multiple = false，表示仅确认当前消息序列号的消息
+             */
+            ConfirmCallback ackCallback = (sequenceNumber, multiple) -> {
+                if (multiple) {
+                    // 返回的是小于等于当前消息序列号的未确认消息，是一个 Map
+                    ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(sequenceNumber, true);
+                    // 清除该部分未确认的消息
+                    confirmed.clear();
+                } else {
+                    // 只清除当前消息序列号的消息
+                    outstandingConfirms.remove(sequenceNumber);
+                }
+            };
+
+            /**
+             * 未收到确认消息的回调
+             */
+            ConfirmCallback nackCallback = (sequenceNumber, multiple) -> {
+                // 异步处理未确认的消息，比如重新发送消息
+                String message = outstandingConfirms.get(sequenceNumber);
+                System.out.println("发布的消息" + message + "未被确认，消息序列号" + sequenceNumber);
+            };
+
+            /**
+             * 添加一个异步确认的监听器
+             * 1. 收到确认消息的回调
+             * 2. 未收到确认消息的回调
+             */
+            channel.addConfirmListener(ackCallback, nackCallback);
+
+            int total = 1000;
+            long begin = System.currentTimeMillis();
+            for (int i = 0; i < total; i++) {
+                String message = "消息" + i;
+                /**
+                 * channel.getNextPublishSeqNo() 获取下一个消息的消息序列号
+                 * 使用 Map 存储全部未确认的消息体，通过消息序列号与消息体进行关联
+                 */
+                outstandingConfirms.put(channel.getNextPublishSeqNo(), message);
+                channel.basicPublish("", queueName, null, message.getBytes(StandardCharsets.UTF_8));
+            }
+
+            long end = System.currentTimeMillis();
+            System.out.println("发布" + total + "个异步确认消息，耗时" + (end - begin) + "ms");
+        }
+    }
+
+}
