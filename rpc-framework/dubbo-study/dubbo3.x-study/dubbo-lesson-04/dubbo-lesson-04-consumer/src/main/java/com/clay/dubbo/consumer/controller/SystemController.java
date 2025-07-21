83f81e7933268fc96d152f963312ac54b4e26683
rqh656418510 <656418510@qq.com>
2025-07-21 12:09:25 +0800

    Update files

--- rpc-framework/dubbo-study/dubbo3.x-study/dubbo-lesson-04/dubbo-lesson-04-consumer/src/main/java/com/clay/dubbo/consumer/controller/SystemController.java
+++ rpc-framework/dubbo-study/dubbo3.x-study/dubbo-lesson-04/dubbo-lesson-04-consumer/src/main/java/com/clay/dubbo/consumer/controller/SystemController.java
@@ -2,7 +2,6 @@ package com.clay.dubbo.consumer.controller;
 
 import com.clay.dubbo.domain.User;
 import com.clay.dubbo.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +10,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/system")
 public class SystemController {
 
     /**
      * 引用 Dubbo 服务
+     * <p> 可以指定版本、负载均衡算法、重试次数、超时毫秒等。
+     * <p> 负载均衡默认为 random，还可以配置为：roundrobin 、leastactive 、consistenthash
+     * <p> 超时毫秒数 timeout 可以统一在 application.yml 进行配置，也可以在具体服务上做个性化配置
+     * <p> 重试次数 retries 可以统一在 application.yml 进行配置，也可以在具体服务上做个性化配置，但是对于新增（插入）等非幂等操作，建议不要重试
      */
     @DubboReference
     private UserService userService;
