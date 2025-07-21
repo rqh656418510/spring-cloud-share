4c0da78681f14a94c48ed62c025c7ea3164308d2
rqh656418510 <656418510@qq.com>
2025-07-21 14:10:33 +0800

    Update files

--- rpc-framework/dubbo-study/dubbo3.x-study/dubbo-lesson-05/dubbo-lesson-05-provider/src/main/java/com/clay/dubbo/provider/service/UserServiceImpl.java
+++ rpc-framework/dubbo-study/dubbo3.x-study/dubbo-lesson-05/dubbo-lesson-05-provider/src/main/java/com/clay/dubbo/provider/service/UserServiceImpl.java
@@ -1,30 +1,22 @@
 package com.clay.dubbo.provider.service;
 
+import com.clay.dubbo.api.DubboUserServiceTriple;
 import com.clay.dubbo.api.HelloRequest;
 import com.clay.dubbo.api.HelloResponse;
-import com.clay.dubbo.api.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
-import org.apache.dubbo.rpc.RpcContext;
-
-import java.util.concurrent.CompletableFuture;
 
 /**
  * 服务提供者（暴露服务）
  */
 @Slf4j
 @DubboService(serialization = "protobuf")
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends DubboUserServiceTriple.UserServiceImplBase {
 
     @Override
     public HelloResponse sayHello(HelloRequest request) {
-        log.info("request from consumer: {}", RpcContext.getContext().getRemoteAddress());
-        return HelloResponse.newBuilder().setMessage("Hello " + request.getName()).build();
-    }
-
-    @Override
-    public CompletableFuture<HelloResponse> sayHelloAsync(HelloRequest request) {
-        return CompletableFuture.completedFuture(sayHello((request)));
+        log.info("Hello, " + request.getName());
+        return HelloResponse.newBuilder().setMessage("Hello, " + request.getName() + "!").build();
     }
 
 }
