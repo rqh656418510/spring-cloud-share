c5392eac19aa79ffb0c070fcdf8195e438702a6a
rqh656418510 <656418510@qq.com>
2025-08-16 21:33:40 +0800

    Update files

--- big-data/zookeeper-study/zookeeper-lesson-03/src/main/java/com/clay/zookeeper/DistributeLockTest.java
+++ big-data/zookeeper-study/zookeeper-lesson-03/src/main/java/com/clay/zookeeper/DistributeLockTest.java
@@ -5,8 +5,8 @@ import java.util.UUID;
 public class DistributeLockTest {
 
     public static void main(String[] args) throws Exception {
-        FairLock();
-        // NonFairLock();
+        // FairLock();
+        NonFairLock();
     }
 
     /**
@@ -22,14 +22,14 @@ public class DistributeLockTest {
                 try {
                     // 抢占锁
                     lock1.lock();
-                    System.out.println("Thread 1 启动，获得锁");
+                    System.out.println("Thread 1 获得锁");
                     Thread.sleep(10 * 1000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
                     // 释放锁
                     lock1.unlock();
-                    System.out.println("Thread 1 退出，释放锁");
+                    System.out.println("Thread 1 释放锁");
                 }
             }
         }).start();
@@ -40,14 +40,14 @@ public class DistributeLockTest {
                 try {
                     // 抢占锁
                     lock2.lock();
-                    System.out.println("Thread 2 启动，获得锁");
+                    System.out.println("Thread 2 获得锁");
                     Thread.sleep(10 * 1000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
                     // 释放锁
                     lock2.unlock();
-                    System.out.println("Thread 2 退出，释放锁");
+                    System.out.println("Thread 2 释放锁");
                 }
             }
         }).start();
@@ -63,17 +63,24 @@ public class DistributeLockTest {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                boolean locked = false;
                 try {
                     // 抢占锁
-                    noneFailLock.tryLock(lockId, 15 * 1000);
-                    System.out.println("Thread 1 启动，获得锁");
-                    Thread.sleep(10 * 1000);
+                    locked = noneFailLock.tryLock(lockId, 15 * 1000);
+                    if (locked) {
+                        System.out.println("Thread 1 获取锁成功");
+                        Thread.sleep(10 * 1000);
+                    } else {
+                        System.out.println("Thread 1 获取锁失败");
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
                     // 释放锁
-                    noneFailLock.unlock(lockId);
-                    System.out.println("Thread 1 退出，释放锁");
+                    if (locked) {
+                        noneFailLock.unlock(lockId);
+                        System.out.println("Thread 1 释放锁");
+                    }
                 }
             }
         }).start();
@@ -86,10 +93,10 @@ public class DistributeLockTest {
                     // 抢占锁
                     locked = noneFailLock.tryLock(lockId, 15 * 1000);
                     if (locked) {
-                        System.out.println("Thread 2 启动，获取锁成功");
+                        System.out.println("Thread 2 获取锁成功");
                         Thread.sleep(10 * 1000);
                     } else {
-                        System.out.println("Thread 2 启动，获取锁失败");
+                        System.out.println("Thread 2 获取锁失败");
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -97,7 +104,7 @@ public class DistributeLockTest {
                     // 释放锁
                     if (locked) {
                         noneFailLock.unlock(lockId);
-                        System.out.println("Thread 2 退出，释放锁");
+                        System.out.println("Thread 2 释放锁");
                     }
                 }
             }
