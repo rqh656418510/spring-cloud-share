a6aa3c40e4d77aa875af0e3aac632c8e60a26417
rqh656418510 <656418510@qq.com>
2025-08-27 15:47:42 +0800

    Update files

--- distributed-scene/distributed-scene-study/distributed-scene-02/eshop-cache-ha/src/main/java/com/clay/eshop/cache/ha/controller/CacheController.java
+++ distributed-scene/distributed-scene-study/distributed-scene-02/eshop-cache-ha/src/main/java/com/clay/eshop/cache/ha/controller/CacheController.java
@@ -1,28 +1,29 @@
 package com.clay.eshop.cache.ha.controller;
 
+import com.clay.eshop.cache.ha.http.HttpClientUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.clay.eshop.cache.ha.http.HttpClientUtils;
 
 /**
  * 缓存服务的接口
  */
 @Controller
+@RequestMapping("/cache")
 public class CacheController {
 
-	@RequestMapping("/change/product")
-	@ResponseBody
-	public String changeProduct(Long productId) {
-		// 拿到一个商品id
-		// 调用商品服务的接口，获取商品id对应的商品的最新数据
-		// 用HttpClient去调用商品服务的http接口
-		String url = "http://127.0.0.1:8082/getProductInfo?productId=" + productId;
-		String response = HttpClientUtils.sendGetRequest(url);
-		System.out.println(response);  
-		
-		return "success";
-	}
-	
+    /**
+     * 更新商品信息的缓存数据
+     */
+    @RequestMapping("/change/product")
+    @ResponseBody
+    public String changeProduct(Long productId) {
+        // 调用商品服务的接口，获取商品ID对应的商品的最新数据，用HttpClient去调用商品服务的Http接口
+        String url = "http://127.0.0.1:9092/product/info?productId=" + productId;
+        String response = HttpClientUtils.sendGetRequest(url);
+        System.out.println(response);
+
+        return "success";
+    }
+
 }
--- distributed-scene/distributed-scene-study/distributed-scene-02/eshop-product-ha/src/main/java/com/clay/eshop/product/ha/controller/ProductController.java
+++ distributed-scene/distributed-scene-study/distributed-scene-02/eshop-product-ha/src/main/java/com/clay/eshop/product/ha/controller/ProductController.java
@@ -8,12 +8,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 商品服务的接口
  */
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
-	@RequestMapping("/getProductInfo")
-	@ResponseBody
-	public String getProductInfo(Long productId) {
-		return "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:00:00\"}";
-	}
-	
+    /**
+     * 商品信息接口
+     */
+    @RequestMapping("/info")
+    @ResponseBody
+    public String info(Long productId) {
+        return "{\"id\": " + productId +
+            ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5" +
+            ".5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:00:00\"}";
+    }
+
 }
