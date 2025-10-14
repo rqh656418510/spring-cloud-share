8ff0f51e272ff51a2d6432bba64ff20c20ced5a7
rqh656418510 <656418510@qq.com>
2025-10-14 13:45:05 +0800

    Update files

---
+++ rpc-framework/dubbo-study/dubbo2.x-study/dubbo-lesson-12/dubbo-lesson-12-producer/src/main/java/com/clay/dubbo/producer/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.clay.dubbo.producer.service.impl;
+
+import com.clay.dubbo.domain.User;
+import com.clay.dubbo.service.UserService;
+import org.apache.dubbo.config.annotation.DubboService;
+
+/**
+ * 暴露 Dubbo 服务
+ */
+@DubboService
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    public User getById(Long id) {
+        try {
+            // 模拟业务耗时处理
+            Thread.sleep(500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new User(id, "Peter", 18);
+    }
+
+}
