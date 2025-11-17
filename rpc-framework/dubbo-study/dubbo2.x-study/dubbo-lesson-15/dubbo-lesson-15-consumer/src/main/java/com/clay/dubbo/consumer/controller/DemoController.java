a37b08f072aacec44c76d90d85063e30e8d580a3
rqh656418510 <656418510@qq.com>
2025-11-17 10:55:11 +0800

    Update files

--- rpc-framework/dubbo-study/dubbo2.x-study/dubbo-lesson-15/dubbo-lesson-15-consumer/src/main/java/com/clay/dubbo/consumer/controller/DemoController.java
+++ rpc-framework/dubbo-study/dubbo2.x-study/dubbo-lesson-15/dubbo-lesson-15-consumer/src/main/java/com/clay/dubbo/consumer/controller/DemoController.java
@@ -15,7 +15,7 @@ public class DemoController {
      * 引用 Dubbo 服务
      * <p> 可以指定版本、负载均衡算法、重试次数、超时毫秒等。
      * <p> 负载均衡默认为 random，还可以配置为：roundrobin 、leastactive 、consistenthash
-     * <p> 重试次数 retries 可以统一在 application.yml 进行配置，但是对于增删改等非幂等操作，建议不要重试
+     * <p> 重试次数 retries 可以统一在 application.yml 进行配置，但是对于插入数据等非幂等操作，建议不要重试
      * <p> 超时毫秒数 timeout 可以统一在 application.yml 进行配置，也可以在具体服务上做个性化配置
      */
     @DubboReference
