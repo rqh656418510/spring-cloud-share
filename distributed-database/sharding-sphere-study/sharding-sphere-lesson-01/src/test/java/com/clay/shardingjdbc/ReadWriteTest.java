3a951f8ae5edc8ce6dbb33c828e1205099d1ea62
rqh656418510 <656418510@qq.com>
2025-12-08 17:29:19 +0800

    Update files

--- distributed-database/sharding-sphere-study/sharding-sphere-lesson-01/src/test/java/com/clay/shardingjdbc/ReadWriteTest.java
+++ distributed-database/sharding-sphere-study/sharding-sphere-lesson-01/src/test/java/com/clay/shardingjdbc/ReadWriteTest.java
@@ -5,6 +5,9 @@ import com.clay.shardingjdbc.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 class ReadWriteTest {
@@ -13,13 +16,38 @@ class ReadWriteTest {
     private UserMapper userMapper;
 
     /**
-     * 写入数据的测试
+     * 读写分离测试
      */
     @Test
     public void testInsert() {
         User user = new User();
         user.setUname("张三丰");
         userMapper.insert(user);
+
+        List<User> list = userMapper.selectList(null);
+    }
+
+    /**
+     * 事务测试 <p>
+     * 当使用 @Transactional 事务注解后，所有 SQL 语句（包括读和写）都只在主库执行
+     */
+    @Test
+    @Transactional
+    public void testTransaction() {
+        User user = new User();
+        user.setUname("李思思");
+        userMapper.insert(user);
+
+        List<User> list = userMapper.selectList(null);
+    }
+
+    /**
+     * 负载均衡测试
+     */
+    @Test
+    public void testLoadBalance() {
+        List<User> users1 = userMapper.selectList(null); // 执行第一次查询，路由到 slave1 库
+        List<User> users2 = userMapper.selectList(null); // 执行第二次查询，路由到 slave2 库
     }
 
 }
