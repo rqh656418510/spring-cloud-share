2ecc128021778835772ca88253de8e3b361cf708
rqh656418510 <656418510@qq.com>
2025-12-12 23:48:07 +0800

    Update files

--- distributed-database/sharding-sphere-study/sharding-sphere-lesson-06/src/main/java/com/clay/shardingjdbc/entity/Dict.java
+++ distributed-database/sharding-sphere-study/sharding-sphere-lesson-06/src/main/java/com/clay/shardingjdbc/entity/Dict.java
@@ -11,7 +11,7 @@ public class Dict {
 
     /**
      * 使用 MyBatis-Plus 的雪花算法来生成 ID <br>
-     * 由于广播表的表结构及其数据在每个数据库中均完全一致，因此可以使用 MyBatis-Plus 的 ASSIGN_ID（基于雪花算法）主键生成策略
+     * 由于广播表的表结构及其表数据在每个数据库中均完全一致，因此可以使用 MyBatis-Plus 的 ASSIGN_ID（基于雪花算法）主键生成策略
      */
     @TableId(type = IdType.ASSIGN_ID)
     private Long id;
