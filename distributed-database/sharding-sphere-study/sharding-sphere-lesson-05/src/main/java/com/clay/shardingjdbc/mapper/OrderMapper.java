e2c8e2fac5bf2e848643f230ae5f67bb119e9c0e
rqh656418510 <656418510@qq.com>
2025-12-12 17:47:34 +0800

    Update files

--- distributed-database/sharding-sphere-study/sharding-sphere-lesson-05/src/main/java/com/clay/shardingjdbc/mapper/OrderMapper.java
+++ distributed-database/sharding-sphere-study/sharding-sphere-lesson-05/src/main/java/com/clay/shardingjdbc/mapper/OrderMapper.java
@@ -12,8 +12,8 @@ import java.util.List;
 public interface OrderMapper extends BaseMapper<Order> {
 
     /**
-     * 多表关联查询 <p>
-     * 查询每个订单的订单号和总订单金额
+     * 查询每个订单的订单号和总订单金额 <p>
+     * 使用绑定表进行多表关联查询时，必须使用分片键（"order_no"）进行关联，否则会出现笛卡尔积关联或跨库关联，从而严重影响查询效率
      */
     @Select({"SELECT o.order_no, SUM(i.price * i.count) AS amount",
         "FROM t_order o JOIN t_order_item i ON o.order_no = i.order_no",
