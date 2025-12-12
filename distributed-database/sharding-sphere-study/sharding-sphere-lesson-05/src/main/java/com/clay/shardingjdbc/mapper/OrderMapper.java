36865e1728c25a17cc06194edb234991dd9e16ad
rqh656418510 <656418510@qq.com>
2025-12-12 17:02:41 +0800

    Update files

--- distributed-database/sharding-sphere-study/sharding-sphere-lesson-05/src/main/java/com/clay/shardingjdbc/mapper/OrderMapper.java
+++ distributed-database/sharding-sphere-study/sharding-sphere-lesson-05/src/main/java/com/clay/shardingjdbc/mapper/OrderMapper.java
@@ -12,7 +12,8 @@ import java.util.List;
 public interface OrderMapper extends BaseMapper<Order> {
 
     /**
-     * 多表关联查询
+     * 多表关联查询 <p>
+     * 查询每个订单的订单号和总订单金额
      */
     @Select({"SELECT o.order_no, SUM(i.price * i.count) AS amount",
         "FROM t_order o JOIN t_order_item i ON o.order_no = i.order_no",
