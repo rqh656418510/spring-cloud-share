41ca31257f24f94a25080145dc05787adcab3ff6
rqh656418510 <656418510@qq.com>
2026-01-23 00:55:04 +0800

    Update files

--- distributed-database/distributed-transaction-study/distributed-transaction-01/fencai-hmily-money-from/src/main/java/com/distributed/transaction/MoneyFromApplication.java
+++ distributed-database/distributed-transaction-study/distributed-transaction-01/fencai-hmily-money-from/src/main/java/com/distributed/transaction/MoneyFromApplication.java
@@ -3,13 +3,15 @@ package com.distributed.transaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @EnableFeignClients
-@SpringBootApplication
 @MapperScan("com.distributed.transaction.mapper")
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MoneyFromApplication {
 
     public static void main(String[] args) {
--- distributed-database/distributed-transaction-study/distributed-transaction-01/fencai-hmily-money-to/src/main/java/com/distributed/transaction/MoneyToApplication.java
+++ distributed-database/distributed-transaction-study/distributed-transaction-01/fencai-hmily-money-to/src/main/java/com/distributed/transaction/MoneyToApplication.java
@@ -3,11 +3,13 @@ package com.distributed.transaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
 @MapperScan("com.distributed.transaction.mapper")
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MoneyToApplication {
 
     public static void main(String[] args) {
