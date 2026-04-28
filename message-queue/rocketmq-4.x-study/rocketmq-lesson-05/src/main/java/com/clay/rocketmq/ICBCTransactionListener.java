cd962978bda0d0d1b1b2d90358931cbf53dcae08
rqh656418510 <656418510@qq.com>
2026-04-28 19:03:12 +0800

    Update files

--- message-queue/rocketmq-4.x-study/rocketmq-lesson-05/src/main/java/com/clay/rocketmq/ICBCTransactionListener.java
+++ message-queue/rocketmq-4.x-study/rocketmq-lesson-05/src/main/java/com/clay/rocketmq/ICBCTransactionListener.java
@@ -13,13 +13,13 @@ public class ICBCTransactionListener implements TransactionListener {
 
     /**
      * 回调操作方法 <br>
-     * 消息预提交成功后，就会触发该方法的执行，用于完成本地事务（分支事务）的执行
+     * 半消息预提交成功后，就会触发该方法的执行，用于完成本地事务（分支事务）的执行
      */
     @Override
     public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
-        System.out.println("消息预提交成功：" + msg);
+        System.out.println("半消息预提交成功：" + msg);
 
-        // 假设接收到 TAGA 的消息就表示扣款操作成功，TAGB 的消息表示扣款失败，TAGC 的消息表示扣款结果不清楚，需要执行状态回查
+        // 假设接收到 TAGA 消息就表示扣款成功，可以提交半消息；TAGB 消息就表示扣款失败，需要回滚半消息；TAGC 消息就表示扣款结果不清楚，需要执行状态回查操作
         if (StringUtils.equals("TAGA", msg.getTags())) {
             return LocalTransactionState.COMMIT_MESSAGE;
         } else if (StringUtils.equals("TAGB", msg.getTags())) {
@@ -33,7 +33,7 @@ public class ICBCTransactionListener implements TransactionListener {
 
     /**
      * 状态回查方法 <br>
-     * 触发状态回查的最常见原因有两个：
+     * 触发状态回查（查询本地事务的执行状态）的最常见原因有两个：
      * (1) 回调操作返回 UNKNWON
      * (2) TC 没有接收到 TM 的最终全局事务确认指令
      */
